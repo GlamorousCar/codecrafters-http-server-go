35df60f6cd62927809948738f0a1cd20cb997ef2
GlamorousCar <YaBoG228holodec@gmail.com>
2024-07-20 02:39:58 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
+
+type httpResponse struct {
+	StatusLine           string
+	StatusCode           string
+	OptionalReasonPhrase string
+	Headers              []byte
+	ResponseBody         []byte
+}
+
+func NewResponse(status, code, reasonPhrase string) *httpResponse {
+	return &httpResponse{
+		StatusLine:           status,
+		StatusCode:           code,
+		OptionalReasonPhrase: reasonPhrase,
+		Headers:              nil,
+		ResponseBody:         nil,
+	}
+}
+
+func (r httpResponse) Response() []byte {
+	res := make([]byte, 0)
+	res = append(res, []byte(r.StatusLine)...)
+	res = append(res, []byte(" ")...)
+	res = append(res, []byte(r.StatusCode)...)
+	res = append(res, []byte(" ")...)
+	res = append(res, []byte(r.OptionalReasonPhrase)...)
+	res = append(res, []byte("\r\n")...)
+	res = append(res, r.Headers...)
+	res = append(res, []byte("\r\n")...)
+	res = append(res, r.ResponseBody...)
+
+	return res
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,9 +51,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	con, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	resp := NewResponse("HTTP/1.1", "200", "OK")
+
+	con.Write(resp.Response())
+	con.Close()
+
 }
